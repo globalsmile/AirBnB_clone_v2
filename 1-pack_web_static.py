3ee411fec5be1b37cadf6d5517b8b241c87ceae6
globalsmile <mohammedbadmus823@gmail.com>
2024-04-20 11:50:44 +0100

    update: fabfile

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 from datetime import datetime
 from fabric.api import local
+from fabric import task
 
-
+@task
 def do_pack():
     """Generates a .tgz archive from the contents
     of the web_static folder of this repository.
